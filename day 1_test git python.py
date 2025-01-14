6c1db93dc614bfb322c936a1be1c3819be5b9337
safiramecca <safira.mecca@ui.ac.id>
2025-01-14 12:57:05 +0700

    fibonacci

--- day 1_test git python.py
+++ day 1_test git python.py
@@ -1,8 +1,23 @@
-print('hello world')
+print('hello aaaa')
 
-1+1
 
-x = 5
-x**2
 
-x
+#u1 = 1
+#u2 = u1 + u1
+#u3 = u2 + u1
+#u4 = u3 + u4 
+
+
+u1 = 1
+u2 = 1
+hasil=[u1,u2]
+n = 5
+
+for i in range(n-2):
+    u3 = u1 + u2
+    hasil.append(u3)
+    u1 = u2
+    u2 = u3
+
+print(hasil)
+
